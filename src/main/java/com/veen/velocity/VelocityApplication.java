79cd4eed982ef1a6829757f8ed167ce688aa7d23
xuerongNanopay <xuerong.aws.ca@gmail.com>
2026-08-06 17:12:27 -0400

    FEAT: initial venn project

---
+++ src/main/java/com/veen/velocity/VelocityApplication.java
@@ -0,0 +1,13 @@
+package com.veen.velocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelocityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelocityApplication.class, args);
+	}
+
+}
